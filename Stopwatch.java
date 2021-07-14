85966484a26c490e351e713736493ec7bcf86550
kanchan-solanke <kanchanssolanke@gmail.com>
2021-07-15 00:31:24 +0530

    elapsed time between two timer1 and 2

---
+++ Stopwatch.java
@@ -0,0 +1,37 @@
+public class StopWatch {
+
+    private final long start;
+    public StopWatch() {
+        start = System.currentTimeMillis();
+    }
+
+
+    public double elapsedTime() {
+        long now = System.currentTimeMillis();
+        return (now - start) / 1000.0;
+    }
+    public static void main(String[] args){
+        int n = 2; //assume value 2
+
+        // sum of square roots of integers from 1 to n using Math.sqrt(x).
+        StopWatch timer1 = new StopWatch();
+        double sum1 = 0;
+        for (int i = 1; i <= n; i++) {
+            sum1 += Math.sqrt(i);
+        }
+        double time1 = timer1.elapsedTime();
+        System.out.println(sum1);
+        System.out.println(time1);
+
+
+        // sum of square roots of integers from 1 to n using Math.pow(x, 0.5).
+        StopWatch timer2 = new StopWatch();
+        double sum2 = 0;
+        for (int i = 1; i <= n; i++) {
+            sum2 += Math.pow(i, 0.5);
+        }
+        double time2 = timer2.elapsedTime();
+        System.out.println(sum2);
+        System.out.println(time2);
+    }
+}
