6f4272ef24cf0918e3a2b94371cc8500673b5e52
kanchan-solanke <kanchanssolanke@gmail.com>
2021-07-14 15:38:38 +0530

    Sqrt is:

---
+++ Sqrt.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Sqrt {
+    public static void main(String[] args){
+        double c = Double.parseDouble(args[0]);
+        double t = c;
+        double epsilon = 1e-15;
+        while((Math.abs(t-c/t)) > epsilon * t) {
+            t = ((c / t) + t) / 2;
+        }
+            System.out.println(t);
+
+    }
+}
+
+
